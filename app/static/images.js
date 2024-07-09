fed7913d6f5f25943e3c1552ba9da95f3c094415
Kalle Lindqvist <kalle@kpod.se>
2024-07-09 15:29:08 +0200

    Consolidate javascript in one file

--- app/static/images.js
+++ app/static/images.js
@@ -10,4 +10,12 @@ function changeImage(e) {
     e.target.focus()
 }
 
+function updateImage() {
+    var img = document.getElementById('image');
+    img.src = '/camera?' + new Date().getTime();
+}
 
+function raceChanged(race) {
+    var img = document.getElementById('image');
+    img.src = '/static/race/' + race + '/image_0001.jpg';
+}
--- app/static/index.js
+++
@@ -1,9 +0,0 @@
-function updateImage() {
-    var img = document.getElementById('image');
-    img.src = '/camera?' + new Date().getTime();
-}
-
-function raceChanged(race) {
-    var img = document.getElementById('image');
-    img.src = '/static/race/' + race + '/image_0001.jpg';
-}
