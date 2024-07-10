b7553b24b02764edfa7e74281c0fd205fb0b0505
Kalle Lindqvist <kalle@kpod.se>
2024-07-10 15:47:06 +0200

    Removed unused code

--- app/static/websocket.js
+++ app/static/websocket.js
@@ -1,11 +1,4 @@
 var socket = io();
-socket.on('connect', function() {
-    console.log("I'm in")
-})
-
-socket.on('message', function(data) {
-    console.log(data)
-})
 
 socket.on('cage', function(data) {
     if (data == 'open') {
