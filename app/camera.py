54770165301d13710ab86623b11ad710fbb2d0cb
Kalle Lindqvist <kalle@kpod.se>
2025-05-03 10:36:02 +0200

    Enable auto exposure and white balance

--- app/camera.py
+++ app/camera.py
@@ -66,15 +66,12 @@ class Camera:
             controls={
                 "FrameDurationLimits": (frame_duration_limit, frame_duration_limit),
                 "FrameRate": frames_per_second,
-                "AeEnable": False,
-                #"AeExposureMode": libcamera.controls.AeExposureModeEnum.Short,
-                #"AeConstraintMode": libcamera.controls.AeConstraintModeEnum.Highlight,
-                #"AeFlickerMode": libcamera.controls.AeFlickerModeEnum.Off,
-                #"AeMeteringMode": libcamera.controls.AeMeteringModeEnum.CentreWeighted,
-                #"AwbEnable": True,  # Enable auto white balance to fix green tint
-                "AnalogueGain": 8.0,  # Higher gain to reduce frame drops
-                "ExposureTime": 1000,  # Longer exposure time to reduce frame drops
-                "Brightness": 0.24
+                "AeEnable": True,
+                "AeExposureMode": libcamera.controls.AeExposureModeEnum.Short,
+                "AeConstraintMode": libcamera.controls.AeConstraintModeEnum.Highlight,
+                "AeFlickerMode": libcamera.controls.AeFlickerModeEnum.Off,
+                "AeMeteringMode": libcamera.controls.AeMeteringModeEnum.CentreWeighted,
+                "AwbEnable": True 
             },
             queue=8  # Further increased queue size for smoother processing
         )
