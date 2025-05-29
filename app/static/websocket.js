f2fe6efa708dd5f4f8bdcad13af93b02e7575a25
Kalle Lindqvist <kalle@kpod.se>
2025-05-29 18:49:03 +0200

    Disable race and image index controls when appropriate

--- app/static/websocket.js
+++ app/static/websocket.js
@@ -11,16 +11,22 @@ socket.on('race', function(data) {
         var img = document.getElementById('image');
         img.src = '/static/active_race.png';
         document.getElementById('ready_button').disabled = true;
+        document.getElementById('race').disabled = true;
+        document.getElementById('image_index').disabled = true;
         document.getElementById('stop_button').disabled = true;
     }
     else if(data === '🟡 Inte redo') {
         window.removeEventListener('beforeunload', beforeUnloadHandler);
         window.location = window.location.href
         document.getElementById('ready_button').disabled = false;
+        document.getElementById('race').disabled = false;
+        document.getElementById('image_index').disabled = false;
         document.getElementById('stop_button').disabled = true;
     }
     else {
         document.getElementById('ready_button').disabled = true;
+        document.getElementById('race').disabled = true;
+        document.getElementById('image_index').disabled = true;
         document.getElementById('stop_button').disabled = false;
     }
 })
